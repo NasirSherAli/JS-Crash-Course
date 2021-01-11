a1fe87425564de37a15446f20033379f0115725e
Letmetry42 <connectwithnasir42@gmail.com>
2021-01-12 04:56:06 +0500

    changes

---
+++ main.js
@@ -0,0 +1,12 @@
+// string, numbers, boolean, null, undefined
+
+const name = 'john';
+const age = 30;
+
+// concatenation
+
+console.log('My name is ' + nam + ' and  I am ' + age);
+
+// Template String
+const hello = 'My name is ${name} and I am ${age}';
+
